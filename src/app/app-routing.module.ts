8956926083752032395f432468d4e551db16f0b6
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-02-26 14:20:30 -0300

    Rota para Tecnico-List

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [ 
-      {path: 'home', component: HomeComponent} // rota filha componente nav
+      { path: 'home', component: HomeComponent}, // rota filha componente nav
+      { path: 'tecnicos', component: TecnicoListComponent}
     ]
   }
 ];
