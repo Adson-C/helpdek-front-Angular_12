9af113f104a9d4dd3b446bc74c592e01da650eff
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-02-28 09:39:26 -0300

    Guard Auth criado

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { API_CONFIG } from './../config/config';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Credenciais } from '../models/credenciais';
@@ -10,6 +11,13 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   authenticate(creds: Credenciais) {
-
+    return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {
+      observe: 'response',
+      responseType: 'text'
+    });
   }
+
+successFuLogin(authToken: string) {
+  localStorage.setItem('token', authToken);
+}  
 }
