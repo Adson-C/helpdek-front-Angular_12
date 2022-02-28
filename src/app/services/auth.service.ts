4547b1f607ca870c08cbb5f6adadfd6b87fa7008
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-02-28 10:15:57 -0300

    Authenticação de Login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,11 +2,14 @@ import { API_CONFIG } from './../config/config';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Credenciais } from '../models/credenciais';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  jwtService: JwtHelperService = new JwtHelperService();
 
   constructor(private http: HttpClient) { }
 
@@ -17,7 +20,16 @@ export class AuthService {
     });
   }
 
-successFuLogin(authToken: string) {
+  successFuLogin(authToken: string) {
   localStorage.setItem('token', authToken);
-}  
+  } 
+
+  isAuthenticated() {
+    let token = localStorage.getItem('token')
+    if (token != null) {
+      return !this.jwtService.isTokenExpired(token)
+    }
+    return false
+  }
+
 }
