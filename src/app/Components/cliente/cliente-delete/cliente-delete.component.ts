022ddb9e3fa9ec10195764308218573f95debfdd
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-03-03 12:12:05 -0300

    CRUD Completo para Cliente

---
+++ src/app/Components/cliente/cliente-delete/cliente-delete.component.ts
@@ -0,0 +1,61 @@
+import { Cliente } from 'src/app/models/cliente';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { ToastrService } from 'ngx-toastr';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cliente-delete',
+  templateUrl: './cliente-delete.component.html',
+  styleUrls: ['./cliente-delete.component.css']
+})
+export class ClienteDeleteComponent implements OnInit {
+
+  cliente: Cliente = {
+    id: '',
+    nome: '',
+    cpf: '',
+    email: '',
+    senha: '',
+    perfis: [],
+    dataCraicao: ''
+  }
+
+  constructor(
+    private service: ClienteService,
+    private toastr: ToastrService,
+    private router: Router,
+    private route: ActivatedRoute,
+    ) { }
+
+  ngOnInit(): void {
+    this.cliente.id = this.route.snapshot.paramMap.get('id');
+    this.findById();
+   }
+
+
+    findById(): void {
+      this.service.findById(this.cliente.id).subscribe(resposta => {
+        resposta.perfis = []
+        this.cliente = resposta;
+      })
+    }
+  
+  delete(): void{
+    this.service.delete(this.cliente.id).subscribe(() => {
+      this.toastr.success('Cliente excluído com sucesso', 'Delete');
+      this.router.navigate(['clientes'])
+    }, ex => {
+      if (ex.error.errors) {
+        ex.error.errors.array.forEach(element => {
+          this.toastr.error(element.message);
+        });
+        
+      }else {
+        this.toastr.error(ex.error.message);
+      }
+      
+    })
+  }
+}
