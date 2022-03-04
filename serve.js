bf171d1ccdb486184f33ee4ecb290cf30fe97c17
Adson Conceição <adsonconcecao@yahoo.com.br>
2022-03-04 12:01:30 -0300

    Ajustes

--- serve.js
+++ serve.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const helpdesk = process.env.npm_package_name;
+const helpdeskApp = process.env.npm_package_name;
 const app = express();
 
-app.use(express.static(`${__dirname}/dist/${helpdesk}`));
+app.use(express.static(`${__dirname}/dist/${helpdeskApp}`));
 
 app.get('/*', (req, res) => {
-res.sendFile(path.join(`${__dirname}/dist/${helpdesk}/index.html`));
+res.sendFile(path.join(`${__dirname}/dist/${helpdeskApp}/index.html`));
 });
 
 app.listen(process.env.PORT || 8080);
